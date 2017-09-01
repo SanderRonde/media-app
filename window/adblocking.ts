82f8ce9467190a90c3f853986ab090880d21aeeb
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-01 23:40:00 +0200

    now blocks ads

--- window/adblocking.ts
+++ window/adblocking.ts
@@ -1,25 +1,52 @@
-import { initialize, isAd, client } from 'is-ad'
+import * as fs from 'fs';
+import * as filterParser from 'abp-filter-parser'
 
 export namespace AdBlocking {
 	export let done: boolean = false;
-	let initialization = initialize().then(() => {
-		done = true;
-		return new Promise<void>((resolve) => {
-			resolve();
-		});
-	});
+	let initialization: Promise<void> = init();
+	const rules: Partial<filterParser.FilterData> = {};
 
-	export async function shouldBlock(url: string): Promise<boolean> {
+	function mapTypes(requestType: string): filterParser.elementTypes {
+		switch (requestType) {
+			case 'script':
+				return filterParser.elementTypes.SCRIPT;
+			case 'stylesheet':
+				return filterParser.elementTypes.STYLESHEET;
+			case 'xhr':
+				return filterParser.elementTypes.XMLHTTPREQUEST;
+			case 'image':
+				return filterParser.elementTypes.IMAGE;
+			case 'subFrame':
+				return filterParser.elementTypes.SUBDOCUMENT;
+			case 'mainFrame':
+				return filterParser.elementTypes.DOCUMENT;
+			case 'object':
+				return filterParser.elementTypes.OBJECT;
+			case 'other':
+			default:
+				return filterParser.elementTypes.OTHER;
+		}
+	}
+
+	export async function shouldBlock(details: Electron.OnBeforeRequestDetails, pageURL: string): Promise<boolean> {
 		await initialization;
-		return isAd(url);
+
+		const domain = new URL(pageURL).hostname;
+		const requestURL = details.url;
+		const type = mapTypes(details.resourceType);
+
+		return filterParser.matches(rules, requestURL, {
+			domain: domain,
+			elementTypeMaskMap: type
+		});
 	}
 
 	export async function handleBlocking(view: Electron.WebviewTag) {
 		await initialization;
 		view.getWebContents().session.webRequest.onBeforeRequest({
-			urls: ['*://*./*']
+			urls: ['*://*./*', '*://*/*']
 		}, async (details, callback) => {
-			const shouldBeBlocked = await shouldBlock(details.url);
+			const shouldBeBlocked = await shouldBlock(details, view.getURL());
 			callback({
 				cancel: shouldBeBlocked
 			});
@@ -28,6 +55,19 @@ export namespace AdBlocking {
 
 	export async function addRule(rule: string) {
 		await initialization;
-		client.parse(rule);
+		filterParser.parse(rule, rules);
+	}
+
+	function init(): Promise<void> {
+		return new Promise<void>((resolve) => {
+			fs.readFile('./adblocking/easylist.txt', 'utf8', (err, easylistTxt) => {
+				if (err) {
+					alert('Failed to find ad blocking list');
+				} else {
+					filterParser.parse(easylistTxt, rules);
+				}
+				resolve()
+			});
+		});
 	}
 }
