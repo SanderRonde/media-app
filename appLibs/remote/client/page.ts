b5840bc343e1ebcf0f07ea4eaa44868e7af69df5
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-07 19:35:05 +0200

    features offline indicator

--- appLibs/remote/client/page.ts
+++ appLibs/remote/client/page.ts
@@ -12,74 +12,94 @@ declare class PaperRipple {
 	upAction(): void;
 }
 
-Array.from(document.querySelectorAll('.rippleTarget')).forEach((rippleTarget: HTMLElement) => {
-	const ripple = new PaperRipple();
-	rippleTarget.appendChild(ripple.$);
+function initRipple() {
+	Array.from(document.querySelectorAll('.rippleTarget')).forEach((rippleTarget: HTMLElement) => {
+		const ripple = new PaperRipple();
+		rippleTarget.appendChild(ripple.$);
 
-	rippleTarget.addEventListener('mousedown', (e) => {
-		ripple.downAction(e);
+		rippleTarget.addEventListener('mousedown', (e) => {
+			ripple.downAction(e);
+		});
+		rippleTarget.addEventListener('mouseup', () => {
+			ripple.upAction();
+		});
 	});
-	rippleTarget.addEventListener('mouseup', () => {
-		ripple.upAction();
-	});
-});
+}
 
-const ws = new WebSocket(`ws://${location.hostname}:${location.port}`);
-ws.onmessage = (event) => {
-	const data = JSON.parse(event.data) as {
-		type: 'statusUpdate';
-		data: {
-			app: string;
-			status: string;
-		};
-	}|{
-		type: 'playUpdate';
-		data: {
-			playing: boolean;
-		}
-	};
-
-	switch (data.type) {
-		case 'playUpdate':
-			const playCont = document.getElementById('playPause');
-			if (!data.data.playing) {
-				playCont.classList.add('pause');
-			} else {
-				playCont.classList.remove('pause');
+function initWs() {
+	const ws = new WebSocket(`ws://${location.hostname}:${location.port}`);
+	ws.onmessage = (event) => {
+		const data = JSON.parse(event.data) as {
+			type: 'statusUpdate';
+			data: {
+				app: string;
+				status: string;
+			};
+		}|{
+			type: 'playUpdate';
+			data: {
+				playing: boolean;
 			}
-			break;
-		case 'statusUpdate':
-			const statusTypeContainer = document.getElementById('statusType');
-			const statusContainer = document.getElementById('status');
+		};
 
-			statusTypeContainer.innerText = data.data.app;
-			statusContainer.innerText = data.data.status;
+		switch (data.type) {
+			case 'playUpdate':
+				const playCont = document.getElementById('playPause');
+				if (!data.data.playing) {
+					playCont.classList.add('pause');
+				} else {
+					playCont.classList.remove('pause');
+				}
+				break;
+			case 'statusUpdate':
+				const statusTypeContainer = document.getElementById('statusType');
+				const statusContainer = document.getElementById('status');
 
-			Array.from(document.querySelectorAll('#switchTypeRow > *')).forEach((switchType) => {
-				switchType.classList.remove('selected');
-			});
+				statusTypeContainer.innerText = data.data.app;
+				statusContainer.innerText = data.data.status;
 
-			document.getElementById(data.data.app).classList.add('selected');
-			break;
+				Array.from(document.querySelectorAll('#switchTypeRow > *')).forEach((switchType) => {
+					switchType.classList.remove('selected');
+				});
+
+				document.getElementById(data.data.app).classList.add('selected');
+				break;
+		}
 	}
 }
 
-([
-	'focus',
-	'lowerVolume',
-	'raiseVolume',
-	'pausePlay',
-	'magicButton',
-	'pause',
-	'pausePlay',
-	'youtubeSubscriptions',
-	'youtubeMusic',
-	'youtubeSearch',
-	'netflix',
-	'up', 'down', 'left', 'right',
-	'toggleVideo'
-]).forEach((externalEvent) => {
-	document.getElementById(externalEvent).addEventListener('tap', () => {
-		fetch(`/api/${externalEvent}`);
+function initListeners() {
+	([
+		'focus',
+		'lowerVolume',
+		'raiseVolume',
+		'pausePlay',
+		'magicButton',
+		'pause',
+		'pausePlay',
+		'youtubeSubscriptions',
+		'youtubeMusic',
+		'youtubeSearch',
+		'netflix',
+		'up', 'down', 'left', 'right',
+		'toggleVideo'
+	]).forEach((externalEvent) => {
+		document.getElementById(externalEvent).addEventListener('tap', () => {
+			fetch(`/api/${externalEvent}`);
+		});
 	});
-});
+}
+
+function initOfflineIndicator() {
+	window.addEventListener('online', () => {
+		document.getElementById('networkStatus').classList.remove('visible');
+	});
+	window.addEventListener('offline', () => {
+		document.getElementById('networkStatus').classList.add('visible');
+	});
+}
+
+initWs();
+initRipple();
+initListeners();
+initOfflineIndicator();
