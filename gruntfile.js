d954cc990c53dee7d2e043c4ec69ba1071a20a7d
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-09 20:31:35 +0200

    removed unused grunt function and its dependency, now compiles before building

--- gruntfile.js
+++ gruntfile.js
@@ -3,20 +3,10 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		exec: {
 			tsc: 'tsc -p ./tsconfig.json --watch false'
-		},
-		zip: {
-			'using-cwd': {
-				cwd: './app',
-				src: ['app/**', 'app/**/*.ts'],
-				dest: 'build/MediaApp.zip'
-			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-exec');
-	grunt.loadNpmTasks('grunt-zip');
 
-	grunt.registerTask('build', ['zip']);
 	grunt.registerTask('compile', ['exec:tsc']);
-	grunt.registerTask('test', []);
 }
