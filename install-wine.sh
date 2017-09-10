917956261a650687d44b2f58421c6866ff187c9c
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-10 02:57:43 +0200

    doesn't build wine from source anymore

--- install-wine.sh
+++ install-wine.sh
@@ -1,6 +1,6 @@
-git clone git://source.winehq.org/git/wine.git
-cd wine
-sudo apt-get update
-sudo apt-get install build-essential
-./configure --enable-win64
-make
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt update
+sudo apt install wine-stable winehq-stable
