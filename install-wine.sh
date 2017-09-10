5ffc35062f8b193686a72dfda482d6f22b7b68a7
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-10 03:14:48 +0200

    installs whatever GLIBCXX_3.4.20 is

--- install-wine.sh
+++ install-wine.sh
@@ -4,3 +4,4 @@ sudo apt-key add Release.key
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 sudo apt update
 sudo apt install wine-stable winehq-stable
+sudo apt-get install libstdc++6
