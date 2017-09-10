d18337c52625ce0e621a47945d0792f8779195dc
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-10 20:59:32 +0200

    moves all files to /out before building

--- gruntfile.js
+++ gruntfile.js
@@ -3,10 +3,23 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		exec: {
 			tsc: 'tsc -p ./tsconfig.json --watch false'
+		},
+		copy: {
+			moveApp: {
+				files: [{
+					expand: true,
+					cwd: 'app/',
+					src: ['**/*.*', '!**/*.ts', '!**/*.map'],
+					dest: 'out/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.registerTask('compile', ['exec:tsc']);
+	grunt.registerTask('move', ['copy:moveApp']);
+	grunt.registerTask('preBuild', ['compile', 'move']);
 }
