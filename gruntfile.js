3f3817030cde069e7eeb0e93f23fa9fe32a9a8b8
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-11 23:31:01 +0200

    reverted grunt change

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		exec: {
-			tsc: 'tsc --project ./tsconfig.json --watch false'
+			tsc: 'tsc -p ./tsconfig.json --watch false'
 		},
 		copy: {
 			moveApp: {
