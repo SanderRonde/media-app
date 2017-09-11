c97ca5f02f409187c46de065882cdeb20106e147
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-11 23:17:49 +0200

    should now execute isTag.js

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,10 +1,13 @@
-node ./scripts/isTag.js
+cd scripts
+node isTag.js
 
 if errorlevel 0 (
+    cd ..
     grunt preBuild
 
     electron-builder -w -p "always"
 ) else (
+    cd ..
     grunt preBuild
 
     electron-builder -w
