330f7b2755353b1f12527aa8fecf911c9b62d81d
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-16 19:11:57 +0200

    restructured app file

--- app/app.ts
+++ app/app.ts
@@ -2,70 +2,204 @@
 import {
 	app, BrowserWindow, ipcMain, dialog
 } from 'electron';
-import * as path from 'path';
 import * as url from 'url';
+import * as path from 'path';
 import { handleUpdates } from './appLibs/updater/updater'
 import { RemoteServer }  from './appLibs/remote/remote';
 import { AdBlocking } from './appLibs/adblocking/adblocking';
 import { registerShortcuts } from './appLibs/shortcuts/shortcuts';
 import { MessageReasons, PassedAlongMessages } from './window/appWindow';
-require('electron-context-menu')({});
 
-const widevine: {
-	load(app: Electron.App, dest: string): boolean;
-	downloadAsync(app: Electron.App, dest: string): Promise<void>;
-} = require('electron-widevinecdm')
-const widevinePath = path.join(app.getPath('appData'), 'widevine');
-const widevineExists = widevine.load(app, widevinePath);
-
-const activeWindowContainer: {
+interface ActiveWindowContainer {
 	activeWindow: Electron.BrowserWindow;
-} = {
-	activeWindow: null
 }
-let activeServer: RemoteServer = null;
 
-const DEBUG = process.argv.filter((arg) => {
-	if (arg.indexOf('--debug-brk=') > -1) {
-		return true;
+namespace MusicApp {
+	const DEBUG = !!process.argv.filter(arg => arg.indexOf('--debug-brk=') > -1).length;
+	const activeWindowContainer: ActiveWindowContainer = {
+		activeWindow: null
 	}
-	return false;
-}).length > 0;
+	let activeServer: RemoteServer = null;	
 
-(() => {
-	async function loadWidevine() {
-		return new Promise(async (resolve) => {
-			if (widevineExists) {
-				resolve();
-			} else {
-				await widevine.downloadAsync(app, widevinePath);
-				app.relaunch();
-				dialog.showMessageBox({
-					message: 'You need to relaunch the app to use widevineCDM',
-					buttons: [
-						'Close now',
-						'Cancel',
-					],
-					defaultId: 0,
-					cancelId: 1,
-				}, (response) => {
-					if (response === 0) {
+	namespace WideVine {
+		export async function load() {
+			const widevine: {
+				load(app: Electron.App, dest: string): boolean;
+				downloadAsync(app: Electron.App, dest: string): Promise<void>;
+			} = require('electron-widevinecdm')
+			const widevinePath = path.join(app.getPath('appData'), 'widevine');
+			const widevineExists = widevine.load(app, widevinePath);
+
+			return new Promise(async (resolve) => {
+				if (widevineExists) {
+					resolve();
+				} else {
+					await widevine.downloadAsync(app, widevinePath);
+					app.relaunch();
+					dialog.showMessageBox({
+						message: 'You need to relaunch the app to use widevineCDM',
+						buttons: [
+							'Close now',
+							'Cancel',
+						],
+						defaultId: 0,
+						cancelId: 1,
+					}, (response) => {
+						if (response === 0) {
+							app.quit();
+						}
+					});
+				}
+			});
+		}
+	}
+
+	namespace Messaging {
+		function respondToMessage<T extends keyof MessageReasons>(identifier: string, response: MessageReasons[T]) {
+			activeWindowContainer.activeWindow && 
+			activeWindowContainer.activeWindow.webContents.send('fromBgPage', {
+				identifier: identifier,
+				data: response,
+				type: 'response'
+			});
+		}
+
+		export function setupListeners() {
+			ipcMain.on('toBgPage', (event: Event, msg: {
+				identifier: string;
+				type: keyof MessageReasons | 'passAlong',
+				respond: boolean;
+				data?: {
+					type: keyof PassedAlongMessages;
+				} | PassedAlongMessages[keyof PassedAlongMessages] | {
+					type: string;
+					data: {
+						app: string;
+						status: string;
+					};
+				}
+			}) => {
+				const { identifier, type, data } = msg;
+				switch (type) {
+					case 'openDevTools':
+						activeWindowContainer.activeWindow.webContents.openDevTools();
+						break;
+					case 'messageServer':
+						activeServer.sendMessage(data as {
+							type: 'statusUpdate';
+							data: {
+								app: string;
+								status: string;
+							};
+						}|{
+							type: 'playUpdate';
+							data: {
+								playing: boolean;
+							}
+						});
+						break;
+					case 'isMinimized':
+						respondToMessage(identifier, 
+							activeWindowContainer.activeWindow && 
+							activeWindowContainer.activeWindow.isMinimized());
+						break;
+					case 'onFullscreened':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.addListener('enter-full-screen', () => {
+							respondToMessage(identifier, null);
+						});
+						break;
+					case 'onMaximized':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.addListener('maximize', () => {
+							respondToMessage(identifier, null);
+						});
+						break;
+					case 'onMinimized':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.addListener('minimize', () => {
+							respondToMessage(identifier, null);
+						});
+						break;
+					case 'onRestored':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.addListener('restore', () => {
+							respondToMessage(identifier, null);
+						});
+						break;
+					case 'isMaximized':
+						respondToMessage(identifier,
+							activeWindowContainer.activeWindow && 
+							activeWindowContainer.activeWindow.isMaximized());
+						break;
+					case 'isFullscreen':
+						respondToMessage(identifier,
+							activeWindowContainer.activeWindow && 
+							activeWindowContainer.activeWindow.isFullScreen());
+						break;
+					case 'restore':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.restore();
+						break;
+					case 'enterFullscreen':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.setFullScreen(true);
+						break;
+					case 'exitFullscreen':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.setFullScreen(false);
+						break;
+					case 'minimize':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.minimize();
+						break;
+					case 'maximize':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.maximize();
+						break;
+					case 'close':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.close();
 						app.quit();
-					}
-				});
-			}
-		});
+						break;
+					case 'quit':
+						app.quit();
+						break;
+					case 'passAlong':
+						activeWindowContainer.activeWindow && 
+						activeWindowContainer.activeWindow.webContents.send('passedAlong', data);
+						break;
+					case 'playStatus':
+						const isPlaying = data === 'play';
+						activeServer.sendMessage({
+							type: 'playUpdate',
+							data: {
+								playing: isPlaying
+							}
+						})
+						break;
+				}
+			});
+		}
 	}
 
-	async function onReady () {
-		await loadWidevine();
+	async function setup() {
+		//Context Menu
+		require('electron-context-menu')({});
 
+		Messaging.setupListeners();
 		AdBlocking.blockAds();
-		
+		handleUpdates();
+		activeServer = new RemoteServer(activeWindowContainer);
+		registerShortcuts(activeWindowContainer);
+		await WideVine.load();
+	}
+
+	async function launch () {		
 		activeWindowContainer.activeWindow = new BrowserWindow({
 			width: 1024,
 			height: 740,
-			icon: path.join(__dirname, 'icons/48.png'),
+			icon: path.join(__dirname, 'icons/32.png'),
 			frame: false,
 			titleBarStyle: 'hidden',
 			title: 'Media App',
@@ -74,10 +208,6 @@ const DEBUG = process.argv.filter((arg) => {
 				plugins: true
 			}
 		});
-		
-		handleUpdates();
-		activeServer = new RemoteServer(activeWindowContainer);
-		registerShortcuts(activeWindowContainer);
 
 		activeWindowContainer.activeWindow.loadURL(url.format({
 			pathname: path.join(__dirname, 'window/main.html'),
@@ -95,135 +225,12 @@ const DEBUG = process.argv.filter((arg) => {
 		}
 	}
 
-	app.on('ready', onReady);
-	app.on('window-all-closed', () => {
-		if (process.platform !== 'darwin') {
-			app.quit();
-		}
-	});
-
-	function respondToMessage<T extends keyof MessageReasons>(identifier: string, response: MessageReasons[T]) {
-		activeWindowContainer.activeWindow && 
-		activeWindowContainer.activeWindow.webContents.send('fromBgPage', {
-			identifier: identifier,
-			data: response,
-			type: 'response'
+	export async function init() {
+		app.on('ready', async () => {
+			await setup();
+			launch();
 		});
 	}
+}
 
-	ipcMain.on('toBgPage', (event: Event, msg: {
-		identifier: string;
-		type: keyof MessageReasons | 'passAlong',
-		respond: boolean;
-		data?: {
-			type: keyof PassedAlongMessages;
-		} | PassedAlongMessages[keyof PassedAlongMessages] | {
-			type: string;
-			data: {
-				app: string;
-				status: string;
-			};
-		}
-	}) => {
-		const { identifier, type, data } = msg;
-		switch (type) {
-			case 'openDevTools':
-				activeWindowContainer.activeWindow.webContents.openDevTools();
-				break;
-			case 'messageServer':
-				activeServer.sendMessage(data as {
-					type: 'statusUpdate';
-					data: {
-						app: string;
-						status: string;
-					};
-				}|{
-					type: 'playUpdate';
-					data: {
-						playing: boolean;
-					}
-				});
-				break;
-			case 'isMinimized':
-				respondToMessage(identifier, 
-					activeWindowContainer.activeWindow && 
-					activeWindowContainer.activeWindow.isMinimized());
-				break;
-			case 'onFullscreened':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.addListener('enter-full-screen', () => {
-					respondToMessage(identifier, null);
-				});
-				break;
-			case 'onMaximized':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.addListener('maximize', () => {
-					respondToMessage(identifier, null);
-				});
-				break;
-			case 'onMinimized':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.addListener('minimize', () => {
-					respondToMessage(identifier, null);
-				});
-				break;
-			case 'onRestored':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.addListener('restore', () => {
-					respondToMessage(identifier, null);
-				});
-				break;
-			case 'isMaximized':
-				respondToMessage(identifier,
-					activeWindowContainer.activeWindow && 
-					activeWindowContainer.activeWindow.isMaximized());
-				break;
-			case 'isFullscreen':
-				respondToMessage(identifier,
-					activeWindowContainer.activeWindow && 
-					activeWindowContainer.activeWindow.isFullScreen());
-				break;
-			case 'restore':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.restore();
-				break;
-			case 'enterFullscreen':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.setFullScreen(true);
-				break;
-			case 'exitFullscreen':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.setFullScreen(false);
-				break;
-			case 'minimize':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.minimize();
-				break;
-			case 'maximize':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.maximize();
-				break;
-			case 'close':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.close();
-				app.quit();
-				break;
-			case 'quit':
-				app.quit();
-				break;
-			case 'passAlong':
-				activeWindowContainer.activeWindow && 
-				activeWindowContainer.activeWindow.webContents.send('passedAlong', data);
-				break;
-			case 'playStatus':
-				const isPlaying = data === 'play';
-				activeServer.sendMessage({
-					type: 'playUpdate',
-					data: {
-						playing: isPlaying
-					}
-				})
-				break;
-		}
-	});
-})();
+MusicApp.init();
