0bc2ea59f427f9233bd8cd7dc8b29bf857012802
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-26 03:06:53 +0200

    private functions now start with _

--- app/window/libs/suggestionBar.ts
+++ app/window/libs/suggestionBar.ts
@@ -2,9 +2,9 @@ import { Helpers, $ } from './helpers';
 
 export class SuggestionBar {
 	private readonly _searchBar: HTMLInputElement = null;
-	private currentSuggestions: Array<string> = [];
-	private selectedSuggestion: number = -1;
-	private originalInput: string = '';
+	private _currentSuggestions: Array<string> = [];
+	private _selectedSuggestion: number = -1;
+	private _originalInput: string = '';
 	private _lastSearch: string = '';
 
 	get searchBar() {
@@ -15,30 +15,30 @@ export class SuggestionBar {
 		return this._lastSearch;
 	}
 
-	private updateInputValue(): string {
-		if (this.selectedSuggestion === -1) {
-			if (this._searchBar.value !== this.originalInput) {
-				this._searchBar.value = this.originalInput;
+	private _updateInputValue(): string {
+		if (this._selectedSuggestion === -1) {
+			if (this._searchBar.value !== this._originalInput) {
+				this._searchBar.value = this._originalInput;
 			}
-			return this.originalInput;
+			return this._originalInput;
 		} else {
-			this._searchBar.value = this.currentSuggestions[this.selectedSuggestion];
-			return this.currentSuggestions[this.selectedSuggestion];
+			this._searchBar.value = this._currentSuggestions[this._selectedSuggestion];
+			return this._currentSuggestions[this._selectedSuggestion];
 		}
 	}
 
-	private updateSelectedSuggestion(index: number): string {
-		this.selectedSuggestion = index;
+	private _updateSelectedSuggestion(index: number): string {
+		this._selectedSuggestion = index;
 		Array.from($(`#${this.els.container}`).getElementsByClassName('suggestions')[0].children).forEach((child) => {
 			child.classList.remove('selected');
 		});
 		if (index !== -1) {
 			$(`#${this.els.container}`).getElementsByClassName('suggestions')[0].children.item(index).classList.add('selected');
 		}
-		return this.updateInputValue();
+		return this._updateInputValue();
 	}
 
-	private async getSuggestions(query: string): Promise<{
+	private async _getSuggestions(query: string): Promise<{
 		value: string;
 		isSuggestion: boolean;
 	}[][]> {
@@ -49,20 +49,20 @@ export class SuggestionBar {
 		return await this.getRef().getSuggestions(query);
 	}
 
-	private async doSearch(query: string) {
+	private async _doSearch(query: string) {
 		this._lastSearch = query;
 		this.getRef().exec(query);
 		this.hideSuggestions();
 	}
 
-	private joinSuggestionParts(suggestionParts: {
+	private _joinSuggestionParts(suggestionParts: {
 		value: string;
 		isSuggestion: boolean;
 	}[]): string {
 		return suggestionParts.map(suggestionPart => suggestionPart.value).join('');
 	}
 
-	private genSuggestionElement(suggestion: {
+	private _genSuggestionElement(suggestion: {
 		value: string;
 		isSuggestion: boolean;
 	}[], index: number): HTMLElement {
@@ -73,20 +73,20 @@ export class SuggestionBar {
 				'highlightedSuggestionPart' : '', value);
 		});
 
-		const joinedSuggestion = this.joinSuggestionParts(suggestion);
+		const joinedSuggestion = this._joinSuggestionParts(suggestion);
 		const container = Helpers.el('div', 'suggestion', textElements, {
 			props: {
 				tabindex: '-1'
 			},
 			listeners: {
 				click: () => {
-					this.updateSelectedSuggestion(index);
-					this.doSearch(joinedSuggestion);		
+					this._updateSelectedSuggestion(index);
+					this._doSearch(joinedSuggestion);		
 				},
 				keydown: (e: KeyboardEvent) => {
 					if (e.key === ' ') {
-						this.updateSelectedSuggestion(index);
-						this.doSearch(joinedSuggestion);
+						this._updateSelectedSuggestion(index);
+						this._doSearch(joinedSuggestion);
 					}
 				}
 			}
@@ -95,27 +95,27 @@ export class SuggestionBar {
 		return container;
 	}
 
-	private async onKeyPress() {
+	private async _onKeyPress() {
 		const value = this._searchBar.value;
-		this.originalInput = value;
+		this._originalInput = value;
 
-		const suggestions = await this.getSuggestions(value);
+		const suggestions = await this._getSuggestions(value);
 		const suggestionsContainer = $(`#${this.els.container}`).getElementsByClassName('suggestions')[0];
 		Array.from(suggestionsContainer.children).forEach((child: HTMLElement) => {
 			child.remove();
 		});
 		suggestions.map((suggestion, index) => {
-			return this.genSuggestionElement(suggestion, index);
+			return this._genSuggestionElement(suggestion, index);
 		}).forEach((suggestionElement) => {
 			suggestionsContainer.appendChild(suggestionElement);
 		});
 
-		this.currentSuggestions = suggestions.map(this.joinSuggestionParts);
-		this.showSuggestions();
+		this._currentSuggestions = suggestions.map(this._joinSuggestionParts);
+		this._showSuggestions();
 
-		this.updateSelectedSuggestion(-1);
+		this._updateSelectedSuggestion(-1);
 
-		if (this.currentSuggestions.length === 0) {
+		if (this._currentSuggestions.length === 0) {
 			this.hideSuggestions();
 		}
 	}
@@ -126,7 +126,7 @@ export class SuggestionBar {
 		return wasHidden;
 	}
 
-	private showSuggestions() {
+	private _showSuggestions() {
 		$(`#${this.els.container}`).classList.remove('suggestionsHidden');
 	}
 
@@ -138,32 +138,32 @@ export class SuggestionBar {
 					this.getRef().hide();
 				}
 			} else if (e.key === ' ' && e.shiftKey) {
-				this.showSuggestions();
+				this._showSuggestions();
 				e.preventDefault();
 				e.stopPropagation();
 			} else if (e.key === 'ArrowDown') {
-				if (this.selectedSuggestion === this.currentSuggestions.length - 1) {
-					this.updateSelectedSuggestion(-1);
+				if (this._selectedSuggestion === this._currentSuggestions.length - 1) {
+					this._updateSelectedSuggestion(-1);
 				} else {
-					this.updateSelectedSuggestion(this.selectedSuggestion + 1);
+					this._updateSelectedSuggestion(this._selectedSuggestion + 1);
 				}
-				this.updateInputValue();
+				this._updateInputValue();
 				e.preventDefault();
 				e.stopPropagation();
 			} else if (e.key === 'ArrowUp') {
-				if (this.selectedSuggestion > -1) {
-					this.updateSelectedSuggestion(this.selectedSuggestion - 1);
+				if (this._selectedSuggestion > -1) {
+					this._updateSelectedSuggestion(this._selectedSuggestion - 1);
 				} else {
-					this.updateSelectedSuggestion(this.currentSuggestions.length - 1);
+					this._updateSelectedSuggestion(this._currentSuggestions.length - 1);
 				}
-				this.updateInputValue();
+				this._updateInputValue();
 				e.preventDefault();
 				e.stopPropagation();
 			} else if (e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'Enter') {
 				//Don't update input
 			} else {
 				window.setTimeout(() => {
-					this.onKeyPress();
+					this._onKeyPress();
 				}, 1);
 			}
 		});
@@ -171,7 +171,7 @@ export class SuggestionBar {
 		$('#searchButton').addEventListener('click', (e) => {
 			e.preventDefault();
 			e.stopPropagation();
-			this.doSearch(this.updateInputValue());
+			this._doSearch(this._updateInputValue());
 		});
 
 		this.hideSuggestions();
