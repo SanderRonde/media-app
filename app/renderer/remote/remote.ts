6b6bddb0ca56cd871d50218bae3f021c5a971c87
SanderRonde <awsdfgvhbjn@gmail.com>
2017-09-27 13:06:48 +0200

    private properties now start with _

--- app/renderer/remote/remote.ts
+++ app/renderer/remote/remote.ts
@@ -68,9 +68,9 @@ class WSHandler {
 }
 
 export class RemoteServer {
-	private httpServer: http.Server;
-	private wsHandler: WSHandler;
-	private lastState: {
+	private _httpServer: http.Server;
+	private _wsHandler: WSHandler;
+	private _lastState: {
 		app: string;
 		status: string;
 		playing: boolean;
@@ -79,7 +79,7 @@ export class RemoteServer {
 		status: '',
 		playing: true
 	}
-	PATH_MAPS = {
+	private readonly _PATH_MAPS = {
 		'/': INDEX_PATH,
 		'/sw-toolbox.js': SW_TOOLBOX_DIR + 'sw-toolbox.js',
 		'/sw-toolbox.js.map': SW_TOOLBOX_DIR + 'sw-toolbox.js.map',
@@ -91,7 +91,7 @@ export class RemoteServer {
 		'/images/192.png': ICONS_DIR + '192.png'
 	}
 
-	private OPTIONS_MAPS: {
+	private readonly _OPTIONS_MAPS: {
 		[key: string]: {
 			path: string;
 			options: {
@@ -104,7 +104,7 @@ export class RemoteServer {
 	} = {
 		[INDEX_PATH]: {
 			path: INDEX_PATH,
-			options: Object.assign(this.lastState, {
+			options: Object.assign(this._lastState, {
 				offline: false
 			})
 		},
@@ -128,7 +128,7 @@ export class RemoteServer {
 		}
 	}
 
-	private isPortAvailable(port: number): Promise<boolean> {
+	private _isPortAvailable(port: number): Promise<boolean> {
 		return new Promise<boolean>((resolve) => {
 			const server = net.createServer().once('error', (err: NodeError) => {
 				if (err.code === 'EADDRINUSE') {
@@ -145,10 +145,10 @@ export class RemoteServer {
 		});
 	}
 
-	private async findUnusedPort(base: number): Promise<number> {
+	private async _findUnusedPort(base: number): Promise<number> {
 		let currentPort = base;
 
-		while (!await this.isPortAvailable(currentPort)) {
+		while (!await this._isPortAvailable(currentPort)) {
 			if (currentPort === 9999) {
 				currentPort = 0;
 			} else if (currentPort === base - 1) {
@@ -161,46 +161,46 @@ export class RemoteServer {
 		return currentPort;
 	}
 
-	private async initServer(port?: number) {
-		this.httpServer = http.createServer(async (req, res) => {
-			const url = this.getURL(req);
+	private async _initServer(port?: number) {
+		this._httpServer = http.createServer(async (req, res) => {
+			const url = this._getURL(req);
 
 			if (url.startsWith('/api/')) {
-				this.handleAPIRequest(url, res);
+				this._handleAPIRequest(url, res);
 			} else {
-				this.handleFileRequest(url, res);
+				this._handleFileRequest(url, res);
 			}
 		});
 
-		port = port || await this.findUnusedPort(REMOTE_PORT);
+		port = port || await this._findUnusedPort(REMOTE_PORT);
 		if (port !== null) {
-			this.httpServer.listen(port, async () => {
+			this._httpServer.listen(port, async () => {
 				if (port !== REMOTE_PORT) {
-					toast(`Hosting server on ${await this.getIp()}:${port} instead`)
-					log(`Hosting server on ${await this.getIp()}:${port} instead`)
+					toast(`Hosting server on ${await this._getIp()}:${port} instead`)
+					log(`Hosting server on ${await this._getIp()}:${port} instead`)
 				} else {
-					log(`HTTP server listening on ${await this.getIp()}:${port}`)
+					log(`HTTP server listening on ${await this._getIp()}:${port}`)
 				}
 			});
 
-			this.wsHandler = new WSHandler(this.httpServer);		
+			this._wsHandler = new WSHandler(this._httpServer);		
 		} else {
 			error('No valid port could be found, not hosting');
-			this.wsHandler = new WSHandler(null);
+			this._wsHandler = new WSHandler(null);
 		}
 	}
 
-	private async shutdown(): Promise<void> {
+	private async _shutdown(): Promise<void> {
 		return new Promise<void>((resolve) => {
-			this.httpServer.close(() => {
+			this._httpServer.close(() => {
 				resolve();
 			});
 		});
 	}
 
 	public async restart(port?: number) {
-		await this.shutdown();
-		await this.initServer(port);
+		await this._shutdown();
+		await this._initServer(port);
 	}
 
 	constructor(public refs: {
@@ -208,10 +208,10 @@ export class RemoteServer {
 		tray: Electron.Tray;
 		DEBUG: boolean;
 	}, public launch: (focus?: boolean) => boolean) {
-		this.initServer();
+		this._initServer();
 	}
 
-	private getIp() {
+	private _getIp() {
 		return new Promise((resolve, reject) => {
 			require('dns').lookup(require('os').hostname(), (err: Error, address: string) => {
 				if (err) {
@@ -223,7 +223,7 @@ export class RemoteServer {
 		});
 	}
 	
-	private respondError(res: http.ServerResponse, url: string, statusCode: keyof typeof STATUS_CODES) {
+	private _respondError(res: http.ServerResponse, url: string, statusCode: keyof typeof STATUS_CODES) {
 		if (LOG_ERROR_REQUESTS) {
 			console.log(`👎 - [${statusCode}] - ${url}`)
 		}
@@ -231,19 +231,19 @@ export class RemoteServer {
 		res.end();
 	}
 
-	private getFileType(file: string): string {
+	private _getFileType(file: string): string {
 		return file.split('.').reverse()[0];
 	}
 
-	private getJadeOptionsForFile(fileName: string): {
+	private _getJadeOptionsForFile(fileName: string): {
 		path: string;
 		options: {
 			[key: string]: any;
 		}
 	} {
-		if (fileName in this.OPTIONS_MAPS) {
-			const OPTIONS_MAPS = this.OPTIONS_MAPS;
-			return this.OPTIONS_MAPS[fileName as keyof typeof OPTIONS_MAPS];
+		if (fileName in this._OPTIONS_MAPS) {
+			const OPTIONS_MAPS = this._OPTIONS_MAPS;
+			return this._OPTIONS_MAPS[fileName as keyof typeof OPTIONS_MAPS];
 		}
 		return {
 			path: fileName,
@@ -251,11 +251,11 @@ export class RemoteServer {
 		};
 	}
 
-	private getRelativeFile(fileName: string): string {
+	private _getRelativeFile(fileName: string): string {
 		return path.join(__dirname, 'client/', fileName)
 	}
 
-	private getFileInfo(fileName: string): Promise<{
+	private _getFileInfo(fileName: string): Promise<{
 		exists: boolean;
 		isDir: boolean;
 	}> {
@@ -283,31 +283,31 @@ export class RemoteServer {
 		});
 	}
 
-	private async renderJade(fileName: string, res: http.ServerResponse): Promise<string> {
-		const { path, options } = this.getJadeOptionsForFile(fileName);
-		const relativePath = this.getRelativeFile(path);
-		const fileInfo = await this.getFileInfo(relativePath);
+	private async _renderJade(fileName: string, res: http.ServerResponse): Promise<string> {
+		const { path, options } = this._getJadeOptionsForFile(fileName);
+		const relativePath = this._getRelativeFile(path);
+		const fileInfo = await this._getFileInfo(relativePath);
 		if (!fileInfo.exists) {
-			this.respondError(res, fileName, '404');
+			this._respondError(res, fileName, '404');
 			return null;
 		} else if (fileInfo.isDir) {
-			this.respondError(res, fileName, '403');
+			this._respondError(res, fileName, '403');
 			return null;
 		}
 		try {
 			return jade.compileFile(relativePath)(options);
 		} catch(e) {
 			console.log('Jade error', e);
-			this.respondError(res, fileName, '500');
+			this._respondError(res, fileName, '500');
 			return null;
 		}
 	}
 
-	private readFile(fileName: string, res: http.ServerResponse, utf8: true): Promise<string>;
-	private readFile(fileName: string, res: http.ServerResponse): Promise<Buffer>;
-	private readFile(fileName: string, res: http.ServerResponse, utf8?: boolean): Promise<string|Buffer> {
+	private _readFile(fileName: string, res: http.ServerResponse, utf8: true): Promise<string>;
+	private _readFile(fileName: string, res: http.ServerResponse): Promise<Buffer>;
+	private _readFile(fileName: string, res: http.ServerResponse, utf8?: boolean): Promise<string|Buffer> {
 		return new Promise<string|Buffer>((resolve, reject) => {
-			fs.readFile(this.getRelativeFile(fileName), (err, data) => {
+			fs.readFile(this._getRelativeFile(fileName), (err, data) => {
 				if (err) {
 					reject(err);
 				} else {			
@@ -317,7 +317,7 @@ export class RemoteServer {
 		});
 	}
 
-	private async awaitPromise<T>(promise: Promise<T>, onReject: (err: Error) => void): Promise<T> {
+	private async _awaitPromise<T>(promise: Promise<T>, onReject: (err: Error) => void): Promise<T> {
 		return new Promise<T>((resolve) => {
 			promise.then((result) => {
 				resolve(result);
@@ -328,27 +328,27 @@ export class RemoteServer {
 		});
 	}
 
-	private async renderData(fileName: string, res: http.ServerResponse): Promise<Buffer|string> {
-		const fileType = this.getFileType(fileName);
+	private async _renderData(fileName: string, res: http.ServerResponse): Promise<Buffer|string> {
+		const fileType = this._getFileType(fileName);
 		switch (fileType) {
 			case 'jade':
 			case 'html':
-				return await this.renderJade(fileName, res);
+				return await this._renderJade(fileName, res);
 		}
-		return await this.awaitPromise(this.readFile(fileName, res), async () => {
-			const fileInfo = await this.getFileInfo(fileName);
+		return await this._awaitPromise(this._readFile(fileName, res), async () => {
+			const fileInfo = await this._getFileInfo(fileName);
 			if (!fileInfo.exists) {
-				this.respondError(res, fileName, '404');
+				this._respondError(res, fileName, '404');
 			} else if (fileInfo.isDir) {
-				this.respondError(res, fileName, '403');
+				this._respondError(res, fileName, '403');
 			} else {
-				this.respondError(res, fileName, '500');
+				this._respondError(res, fileName, '500');
 			}
 		});
 	}
 	
-	private async serveFile(url: string, res: http.ServerResponse) {
-		const data = await this.renderData(url, res);
+	private async _serveFile(url: string, res: http.ServerResponse) {
+		const data = await this._renderData(url, res);
 		if (data !== null) {
 			if (LOG_REQUESTS) {
 				console.log(`👌 - [200] - ${url}`);
@@ -358,11 +358,11 @@ export class RemoteServer {
 		}
 	}
 	
-	private async handleFileRequest(url: string, res: http.ServerResponse) {
-		this.serveFile(url, res);
+	private async _handleFileRequest(url: string, res: http.ServerResponse) {
+		this._serveFile(url, res);
 	}
 	
-	private sendCommand(command: keyof MessageReasons | EXTERNAL_EVENT | ARG_EVENT, data?: string) {
+	private _sendCommand(command: keyof MessageReasons | EXTERNAL_EVENT | ARG_EVENT, data?: string) {
 		//Launch the app if it isn't running yet
 		const didLaunch = this.launch(false);
 		if (didLaunch) {
@@ -376,12 +376,12 @@ export class RemoteServer {
 		}, this.refs.activeWindow.webContents);
 	}
 	
-	private async handleAPIRequest(url: string, res: http.ServerResponse) {
+	private async _handleAPIRequest(url: string, res: http.ServerResponse) {
 		const command = url.split('/api/').slice(1).join('/api/') as EXTERNAL_EVENT;
 		const partialCommand = command.split('/')[0] as EXTERNAL_EVENT;
 	
 		if (EXTERNAL_EVENTS.indexOf(command) > -1) {
-			this.sendCommand(command);
+			this._sendCommand(command);
 			if (LOG_REQUESTS) {
 				console.log(`👌 - [200] - ${url}`);
 			}
@@ -390,32 +390,32 @@ export class RemoteServer {
 			}));
 			res.end();
 		} else if (ARG_EVENTS.indexOf(partialCommand as ARG_EVENT) > -1) {
-			this.sendCommand(partialCommand, decodeURIComponent(command.split('/').slice(1).join('/')));
+			this._sendCommand(partialCommand, decodeURIComponent(command.split('/').slice(1).join('/')));
 		} else {
-			this.respondError(res, url, '500');
+			this._respondError(res, url, '500');
 		}
 	}
 	
-	private getURL(req: http.IncomingMessage): string {
+	private _getURL(req: http.IncomingMessage): string {
 		const url = urlLib.parse(req.url).pathname;
-		const pathMaps = this.PATH_MAPS;
-		if (url in this.PATH_MAPS) {
-			return this.PATH_MAPS[url as keyof typeof pathMaps];
+		const pathMaps = this._PATH_MAPS;
+		if (url in this._PATH_MAPS) {
+			return this._PATH_MAPS[url as keyof typeof pathMaps];
 		}
 		return url;
 	}
 
-	private updateLastState(state: WSMessage) {
+	private _updateLastState(state: WSMessage) {
 		if (state.type === 'playUpdate') {
-			this.lastState.playing = state.data.playing;
+			this._lastState.playing = state.data.playing;
 		} else {
-			this.lastState.app = state.data.app;
-			this.lastState.status = state.data.status;
+			this._lastState.app = state.data.app;
+			this._lastState.status = state.data.status;
 		}
 	}
 
-	sendMessage(message: WSMessage) {
-		this.updateLastState(message);		
-		this.wsHandler.sendMessage(message);
+	public sendMessage(message: WSMessage) {
+		this._updateLastState(message);		
+		this._wsHandler.sendMessage(message);
 	}
 }
