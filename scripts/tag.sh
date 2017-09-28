18429bf7f720b671c0f0f44d57acab2306dac615
Travis CI <builds@travis-ci.org>
2017-09-29 00:11:08 +0200

    executes scripts relative to position of tag.sh

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -4,7 +4,7 @@ git config user.name "Travis CI"
 git config user.email "builds@travis-ci.org"
 git config --global push.default simple
 
-node ./isTag.js && \
+node $(dirname $0)/isTag.js && \
   (git tag "v${TRAVIS_COMMIT_MESSAGE}" && \
   git push "https://${GITHUB_ACCESS_TOKEN}@github.com/SanderRonde/media-app.git" --tags --quiet > /dev/null) && \
   node createRelease.js
