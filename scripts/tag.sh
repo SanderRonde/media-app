fdd665e133f0e418e2ee1adae1f597eeecba8cc9
Travis CI <builds@travis-ci.org>
2017-09-29 00:17:26 +0200

    should not exit with nonzero exit code now

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -6,7 +6,7 @@ git config user.name "Travis CI"
 git config user.email "builds@travis-ci.org"
 git config --global push.default simple
 
-node $(dirname $0)/isTag.js && \
+(node $(dirname $0)/isTag.js && \
   (git tag "v${TRAVIS_COMMIT_MESSAGE}" && \
   git push "https://${GITHUB_ACCESS_TOKEN}@github.com/SanderRonde/media-app.git" --tags --quiet > /dev/null) && \
-  node createRelease.js
+  node createRelease.js && echo "Created tag") || echo "Did not create tag"
