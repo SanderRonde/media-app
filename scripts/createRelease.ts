d956cc77eff22da89e8abaaa7c61a4165b0d4cf2
SanderRonde <awsdfgvhbjn@gmail.com>
2017-10-13 23:38:25 +0200

    regained faith in automatic tagging/releasing/publishing

---
+++ scripts/createRelease.ts
@@ -0,0 +1,139 @@
+let VERSION = process.env.TRAVIS_COMMIT_MESSAGE;
+if (VERSION.startsWith('v')) {
+	VERSION = VERSION.slice(1);
+}
+const fs = require('fs');
+const path = require('path');
+
+import GithubAPI = require('github')
+
+interface User {
+	login: string;
+	id: number;
+	avatar_url: string;
+	gravatar_id: string;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url: string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type: string;
+	site_admin: boolean;
+}
+
+interface Asset {
+	url: string;
+	browser_download_url: string;
+	id: number;
+	name: string;
+	label: string;
+	state: string;
+	content_type: string;
+	size: number;
+	download_count: number;
+	created_at: string;
+	updated_at: string;
+	uploader: User;
+}
+
+type GithubMeta<T> = {
+	meta: {
+		'x-ratelimit-limit': string;
+		'x-ratelimit-remaining': string,
+		'x-ratelimit-reset': string,
+		'x-oauth-scopes': string,
+		'x-github-request-id': string,
+		'x-github-media-type': string,
+		'last-modified': string,
+		etag: string,
+		status: string
+	};
+	data: T;
+}
+
+interface Release {
+	url: string;
+	html_url: string;
+	assets_url: string;
+	upload_url: string;
+	tarball_url: string;
+	zipball_url: string;
+	id: number;
+	tag_name: string;
+	target_commitish: string;
+	name: string;
+	body: string;
+	draft: boolean;
+	prerelease: boolean;
+	created_at: string;
+	published_at: string;
+	author: User;
+	assets: Asset[];
+}
+
+const gh = new GithubAPI({
+	headers: {
+		'User-Agent': 'SanderRonde'
+	},
+	Promise: Promise
+});
+
+gh.authenticate({
+	type: 'token',
+	token: process.env.GH_ACCESS_TOKEN
+});
+
+async function main() {
+	//Check if it already exists
+	const allReleases: GithubMeta<Release[]> = await gh.repos.getReleases({
+		owner: 'SanderRonde',
+		page: 0,
+		per_page: 20,
+		repo: 'media-app'
+	});
+
+	for (let release of allReleases.data) {
+		if (release.tag_name === `v${VERSION}`) {
+			//Already exists
+			console.log('Already exists');
+			return;
+		}	
+	}
+
+	const fullChangelog: {
+		[key: string]: string[]
+	} = JSON.parse(fs.readFileSync(path.join(__dirname, './changelog.json'), 'utf8'));
+	if (!(VERSION in fullChangelog)) {
+		throw new Error('No release notes for this version exist');
+	} else {
+		const releaseNotes = fullChangelog[VERSION].map((releaseNote) => {
+			return `* ${releaseNote}`
+		}).join('\n');
+
+		await gh.repos.createRelease({
+			owner: 'SanderRonde',
+			repo: 'media-app',
+			tag_name: `v${VERSION}`,
+			body: releaseNotes,
+			draft: true,
+			name: VERSION,
+			prerelease: false,
+			target_commitish: process.env.TRAVIS_COMMIT
+		});
+	}
+}
+
+try{
+	main().then(() => {
+		process.exit(0);
+	});
+} catch(e) {
+	console.log('Something went wrong creating a tag (probably with the API)', e);
+	process.exit(1);
+}
---
+++ scripts/tag.ts
@@ -0,0 +1,88 @@
+const semver = require('semver');
+import GithubAPI = require('github');
+
+const commitMessage = process.env.TRAVIS_COMMIT_MESSAGE || '';
+let VERSION = process.env.TRAVIS_COMMIT_MESSAGE;
+if (VERSION.startsWith('v')) {
+	VERSION = VERSION.slice(1);
+}
+
+interface Tag {
+	name: string;
+	commit: {
+		sha: string;
+		url: string;
+	}
+	zipball_url: string;
+	tarball_url: string;
+}
+
+const validTag = !!semver.valid(commitMessage);
+
+async function main() {
+	if (!validTag) {
+		console.log('Invalid tag')
+		return;
+	}
+		
+	//If not tagged yet, do so
+	const gh = new GithubAPI({
+		headers: {
+			'User-Agent': 'SanderRonde'
+		},
+		Promise: Promise
+	});
+
+	gh.authenticate({
+		type: 'token',
+		token: process.env.GH_ACCESS_TOKEN
+	});
+
+	const tags: Tag[] = await gh.gitdata.getTags({
+		owner: 'SanderRonde',
+		repo: 'media-app'
+	});
+
+	let tagExists: boolean = false;
+	for (let i = 0; i < tags.length; i++) {
+		if (tags[i].name.indexOf(commitMessage.slice(1)) > -1) {
+			tagExists = true;
+		}
+	}
+
+	if (tagExists) {
+		console.log('Tag exists');
+		return;
+	}
+
+	await gh.gitdata.createTag({
+		owner: 'SanderRonde',
+		repo: 'media-app',
+		tag: `v${VERSION}`,
+		message: VERSION,
+		object: process.env.TRAVIS_COMMIT,
+		type: 'commit',
+		tagger: JSON.stringify({
+			name: 'Sander Ronde',
+			email: 'awsdfgvhbjn@gmail.com',
+			date: new Date().toISOString()
+		})
+	});
+
+	await gh.gitdata.createReference({
+		owner: 'SanderRonde',
+		repo: 'media-app',
+		sha: process.env.TRAVIS_COMMIT,
+		ref: `refs/tags/v${VERSION}`
+	});
+}
+
+try {
+	main().then(() => {
+		//Exits with 0 when valid tag, 1 if not
+		process.exit(~~validTag);
+	});
+} catch(e) {
+	console.log('Something went wrong creating a tag (probably with the API)', e);
+	process.exit(1);
+}
